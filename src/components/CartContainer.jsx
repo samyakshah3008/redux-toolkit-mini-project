942e420b96c0ffa014b257bc20e03bfefac4c3ea
Samyak Shah <samyakshah3008@gmail.com>
2022-06-23 01:00:40 +0530

    Cart Container component add

---
+++ src/components/CartContainer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { openModal } from "../features/modal/modalSlice";
+import CartItem from "./CartItem";
+
+export default function CartContainer() {
+  const dispatch = useDispatch();
+
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2> Your Bag </h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <section className="cart">
+        <header>
+          <h2>Your Bag</h2>
+        </header>
+        <div>
+          {cartItems.map((item) => {
+            return <CartItem key={item.id} {...item}></CartItem>;
+          })}
+        </div>
+        <footer>
+          <hr />
+          <div className="cart-total">
+            <h4>
+              Total <span> ${total}</span>
+            </h4>
+          </div>
+          <button
+            className="btn clear-btn"
+            onClick={() => dispatch(openModal())}
+          >
+            Clear Cart
+          </button>
+        </footer>
+      </section>
+    </>
+  );
+}
